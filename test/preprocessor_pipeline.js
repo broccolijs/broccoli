36cc795bb40f3a23f741d6955013b78e53765a53
Jo Liss <joliss42@gmail.com>
2013-12-23 17:46:03 +0000

    Add tests for PreprocessorPipeline

---
+++ test/preprocessor_pipeline.js
@@ -0,0 +1,100 @@
+var fs = require('fs')
+var test = require('tap').test
+var temp = require('temp'); temp.track()
+var broccoli = require('../')
+var testHelpers = require('./test_helpers')
+
+var PreprocessorPipeline = broccoli.PreprocessorPipeline
+var Preprocessor = broccoli.Preprocessor
+
+test('PreprocessorPipeline', function (t) {
+  function transform (pipeline, srcDir, callback) {
+    var destDir = temp.mkdirSync({ prefix: 'preprocessor-pipeline-dest-', suffix: '.tmp', dir: '.' })
+    pipeline.transform(srcDir, destDir, function (err) {
+      if (err) throw err
+      callback(destDir)
+    })
+  }
+
+  // This preprocessor adds ' [name]' to file contents
+  TestPreprocessor.prototype = Object.create(Preprocessor.prototype)
+  TestPreprocessor.prototype.constructor = TestPreprocessor
+  function TestPreprocessor (options) {
+    this.name = options.name
+    this.extensions = options.extensions
+    this.targetExtension = options.targetExtension
+    this.callCount = 0
+  }
+
+  TestPreprocessor.prototype.process = function (srcFilePath, destFilePath, callback) {
+    var contents = fs.readFileSync(srcFilePath)
+    fs.writeFileSync(destFilePath, contents + ' [' + this.name + ']')
+    this.callCount += 1
+    callback()
+  }
+
+  test('processes depending on extension', function (t) {
+    var jsPreprocessor = new TestPreprocessor({
+      name: 'js',
+      extensions: ['js'],
+      targetExtension: 'js'
+    })
+    var aPreprocessor = new TestPreprocessor({
+      name: 'a',
+      extensions: ['a'],
+      targetExtension: 'b'
+    })
+    var bPreprocessor = new TestPreprocessor({
+      name: 'b',
+      extensions: ['b'],
+      targetExtension: 'c'
+    })
+
+    var pipeline = testHelpers.setupComponent(new PreprocessorPipeline)
+      .addPreprocessor(aPreprocessor)
+      .addPreprocessor(bPreprocessor)
+      .addPreprocessor(jsPreprocessor)
+
+    var srcDir = testHelpers.makeTree({
+      'x/y.js': 'y.js contents',
+      'x/y.a': 'y.a contents',
+      'x/y.foo': 'y.foo contents'
+    })
+
+    transform(pipeline, srcDir, function (destDir) {
+      var files = fs.readdirSync(destDir + '/x')
+      files.sort()
+      t.deepEqual(files, ['y.c', 'y.foo', 'y.js'])
+
+      t.equal(
+        fs.readFileSync(destDir + '/x/y.js').toString(),
+        'y.js contents [js]',
+        'preprocessor is applied')
+      t.equal(
+        fs.readFileSync(destDir + '/x/y.c').toString(),
+        'y.a contents [a] [b]',
+        'multiple preprocessors are applied')
+      t.equal(
+        fs.readFileSync(destDir + '/x/y.foo').toString(),
+        'y.foo contents',
+        'unmatched files are passed through')
+      t.equal(aPreprocessor.callCount, 1)
+      t.equal(bPreprocessor.callCount, 1)
+      t.equal(jsPreprocessor.callCount, 1)
+
+      test('files are cached', function (t) {
+        transform(pipeline, srcDir, function (destDir) {
+          // counts are still 1
+          t.equal(aPreprocessor.callCount, 1)
+          t.equal(bPreprocessor.callCount, 1)
+          t.equal(jsPreprocessor.callCount, 1)
+          t.end()
+        })
+      })
+
+      t.end()
+    })
+  })
+
+  t.end()
+})
---
+++ test/test_helpers.js
@@ -0,0 +1,26 @@
+var fs = require('fs')
+var temp = require('temp'); temp.track()
+var path = require('path')
+var mkdirp = require('mkdirp')
+
+module.exports.makeTree = function (tree) {
+  var dir = temp.mkdirSync({ prefix: 'tree-', suffix: '.tmp', dir: '.' })
+  Object.keys(tree).forEach(function (relativePath) {
+    var content = tree[relativePath]
+    mkdirp.sync(path.join(dir, path.dirname(relativePath)))
+    fs.writeFileSync(path.join(dir, relativePath), content)
+  })
+  return dir
+}
+
+var cacheDir
+var tmpDir
+module.exports.setupComponent = function (component) {
+  cacheDir = cacheDir || temp.mkdirSync({ prefix: 'component-cache-dir-', suffix: '.tmp', dir: '.' })
+  tmpDir = tmpDir || temp.mkdirSync({ prefix: 'component-tmp-dir-', suffix: '.tmp', dir: '.' })
+  component.setup({
+    cacheDir: cacheDir,
+    tmpDir: tmpDir
+  })
+  return component
+}
