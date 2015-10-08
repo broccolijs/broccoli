f5a1f3ddb1a01a4a9c312be12f1efed2b6d38611
Jo Liss <joliss42@gmail.com>
2015-10-08 21:36:14 +0000

    Migrate tests to mocha and chai

--- test/builder_test.js
+++ test/builder_test.js
@@ -1,20 +1,21 @@
-var test = require('tap').test
 var broccoli = require('..')
 var Builder = broccoli.Builder
+var chai = require('chai'), expect = chai.expect
+var chaiAsPromised = require('chai-as-promised'); chai.use(chaiAsPromised)
 var RSVP = require('rsvp')
 
 RSVP.on('error', function(error) {
   throw error
 })
 
-function countingTree (readFn) {
+function countingTree(readFn) {
   return {
-    read: function (readTree) {
+    read: function(readTree) {
       this.readCount++
       return readFn.call(this, readTree)
     },
     readCount: 0,
-    cleanup: function () {
+    cleanup: function() {
       var self = this;
 
       return RSVP.resolve()
@@ -26,140 +27,125 @@ function countingTree (readFn) {
   }
 }
 
-
-test('Builder', function (t) {
-  test('core functionality', function (t) {
-    t.end()
-
-    test('build', function (t) {
-      test('passes through string tree', function (t) {
+describe('Builder', function() {
+  describe('core functionality', function() {
+    describe('build', function() {
+      it('passes through string tree', function() {
         var builder = new Builder('someDir')
-        builder.build().then(function (hash) {
-          t.equal(hash.directory, 'someDir')
-          t.end()
-        })
+        return expect(builder.build()).to.eventually.have.property('directory', 'someDir')
       })
 
-      test('calls read on the given tree object', function (t) {
+      it('calls read on the given tree object', function() {
         var builder = new Builder({
-          read: function (readTree) { return 'someDir' }
+          read: function(readTree) { return 'someDir' }
         })
-        builder.build().then(function (hash) {
-          t.equal(hash.directory, 'someDir')
-          t.end()
-        })
+        return expect(builder.build()).to.eventually.have.property('directory', 'someDir')
       })
-
-      t.end()
     })
 
-    test('readTree deduplicates', function (t) {
-      var subtree = new countingTree(function (readTree) { return 'foo' })
+    it('readTree deduplicates', function() {
+      var subtree = new countingTree(function(readTree) { return 'foo' })
       var builder = new Builder({
-        read: function (readTree) {
-          return readTree(subtree).then(function (hash) {
+        read: function(readTree) {
+          return readTree(subtree).then(function(hash) {
             var dirPromise = readTree(subtree) // read subtree again
-            t.ok(dirPromise.then, 'is promise, not string')
+            expect(dirPromise.then).to.be.a('function')
             return dirPromise
           })
         }
       })
-      builder.build().then(function (hash) {
-        t.equal(hash.directory, 'foo')
-        t.equal(subtree.readCount, 1)
-        t.end()
+      return builder.build().then(function(hash) {
+        expect(hash.directory).to.equal('foo')
+        expect(subtree.readCount).to.equal(1)
       })
     })
 
-    test('cleanup', function (t) {
-      test('is called on all trees called ever', function (t) {
-        var tree = countingTree(function (readTree) {
+    describe('cleanup', function() {
+      it('is called on all trees called ever', function() {
+        var tree = countingTree(function(readTree) {
           // Interesting edge case: Read subtree1 on the first read, subtree2 on
           // the second
           return readTree(this.readCount === 1 ? subtree1 : subtree2)
         })
-        var subtree1 = countingTree(function (readTree) { return 'foo' })
-        var subtree2 = countingTree(function (readTree) { throw new Error('bar') })
+        var subtree1 = countingTree(function(readTree) { return 'foo' })
+        var subtree2 = countingTree(function(readTree) { throw new Error('bar') })
         var builder = new Builder(tree)
-        builder.build().then(function (hash) {
-          t.equal(hash.directory, 'foo')
-          builder.build().catch(function (err) {
-            t.equal(err.message, 'bar')
+        return builder.build().then(function(hash) {
+          expect(hash.directory).to.equal('foo')
+          builder.build().catch(function(err) {
+            expect(err.message).to.equal('bar')
             return builder.cleanup()
           })
           .then(function() {
-            t.equal(tree.cleanupCount, 1)
-            t.equal(subtree1.cleanupCount, 1)
-            t.equal(subtree2.cleanupCount, 1)
-            t.end()
+            expect(tree.cleanupCount).to.equal(1)
+            expect(subtree1.cleanupCount).to.equal(1)
+            expect(subtree2.cleanupCount).to.equal(1)
           })
         })
       })
-
-      t.end()
     })
   })
 
-  test('tree graph', function (t) {
-    var parent = countingTree(function (readTree) {
-      return readTree(child).then(function (dir) {
-        return new RSVP.Promise(function (resolve, reject) {
+  it('tree graph', function() {
+    var parent = countingTree(function(readTree) {
+      return readTree(child).then(function(dir) {
+        return new RSVP.Promise(function(resolve, reject) {
           setTimeout(function() { resolve('parentTreeDir') }, 30)
         })
       })
     })
 
-    var child = countingTree(function (readTree) {
-      return readTree('srcDir').then(function (dir) {
-        return new RSVP.Promise(function (resolve, reject) {
+    var child = countingTree(function(readTree) {
+      return readTree('srcDir').then(function(dir) {
+        return new RSVP.Promise(function(resolve, reject) {
           setTimeout(function() { resolve('childTreeDir') }, 20)
         })
       })
     })
 
-    var timeEqual = function (a, b) {
-      t.equal(typeof a, 'number')
+    var timeEqual = function(a, b) {
+      expect(a).to.be.a('number')
 
       // do not run timing assertions in Travis builds
       // the actual results of process.hrtime() are not
       // reliable
       if (process.env.CI !== 'true') {
-        t.ok(a >= b - 5e6 && a <= b + 5e6, a + ' should be within ' + b + ' +/- 5e6')
+        expect(a).to.be.within(b - 5e6, b + 5e6)
       }
     }
 
     var builder = new Builder(parent)
-    builder.build().then(function (hash) {
-      t.equal(hash.directory, 'parentTreeDir')
+    return builder.build().then(function(hash) {
+      expect(hash.directory).to.equal('parentTreeDir')
       var parentNode = hash.graph
-      t.equal(parentNode.directory, 'parentTreeDir')
-      t.equal(parentNode.tree, parent)
+      expect(parentNode.directory).to.equal('parentTreeDir')
+      expect(parentNode.tree).to.equal(parent)
       timeEqual(parentNode.totalTime, 50e6)
       timeEqual(parentNode.selfTime, 30e6)
-      t.equal(parentNode.subtrees.length, 1)
+      expect(parentNode.subtrees.length).to.equal(1)
       var childNode = parentNode.subtrees[0]
-      t.equal(childNode.directory, 'childTreeDir')
-      t.equal(childNode.tree, child)
+      expect(childNode.directory).to.equal('childTreeDir')
+      expect(childNode.tree).to.equal(child)
       timeEqual(childNode.totalTime, 20e6)
       timeEqual(childNode.selfTime, 20e6)
-      t.equal(childNode.subtrees.length, 1)
+      expect(childNode.subtrees.length).to.equal(1)
       var leafNode = childNode.subtrees[0]
-      t.equal(leafNode.directory, 'srcDir')
-      t.equal(leafNode.tree, 'srcDir')
-      t.equal(leafNode.totalTime, 0)
-      t.equal(leafNode.selfTime, 0)
-      t.equal(leafNode.subtrees.length, 0)
-      t.end()
+      expect(leafNode.directory).to.equal('srcDir')
+      expect(leafNode.tree).to.equal('srcDir')
+      expect(leafNode.totalTime).to.equal(0)
+      expect(leafNode.selfTime).to.equal(0)
+      expect(leafNode.subtrees.length).to.equal(0)
     })
   })
 
-  test('string tree callback', function (t) {
+  it('string tree callback', function() {
     var builder = new Builder('fooDir')
-    builder.build(function willReadStringTree (dir) {
-      t.equal(dir, 'fooDir')
-      t.end()
+    var callbackCalled = false
+    return builder.build(function willReadStringTree(dir) {
+      expect(dir).to.equal('fooDir')
+      callbackCalled = true
+    }).then(function() {
+      expect(callbackCalled).to.be.ok
     })
   })
-
-  t.end()
 })
