8a22f960ec34cbaedb6b61d78e1d89454ff0733f
Stefan Penner <stefan.penner@gmail.com>
2017-11-22 12:01:45 -0800

    More ES6 Classes

--- test/plugins.js
+++ test/plugins.js
@@ -9,81 +9,74 @@ const symlinkOrCopySync = require('symlink-or-copy').sync;
 module.exports = function(Plugin) {
   const plugins = {};
 
-  plugins.NoopPlugin = NoopPlugin;
-  NoopPlugin.prototype = Object.create(Plugin.prototype);
-  NoopPlugin.prototype.constructor = NoopPlugin;
-  function NoopPlugin(inputNodes, options) {
-    Plugin.call(this, inputNodes || [], options);
-  }
-  NoopPlugin.prototype.build = function() {};
-
-  // This plugin writes foo.js into its outputPath
-  plugins.VeggiesPlugin = VeggiesPlugin;
-  VeggiesPlugin.prototype = Object.create(Plugin.prototype);
-  VeggiesPlugin.prototype.constructor = VeggiesPlugin;
-  function VeggiesPlugin(inputNodes, options) {
-    Plugin.call(this, inputNodes || [], options);
-  }
-  VeggiesPlugin.prototype.build = function() {
-    fs.writeFileSync(this.outputPath + '/veggies.txt', 'tasty');
+  plugins.NoopPlugin = class NoopPlugin extends Plugin {
+    build() {}
   };
 
-  plugins.MergePlugin = MergePlugin;
-  MergePlugin.prototype = Object.create(Plugin.prototype);
-  MergePlugin.prototype.constructor = MergePlugin;
-  function MergePlugin(inputNodes, options) {
-    Plugin.call(this, inputNodes, options);
-  }
-  MergePlugin.prototype.build = function() {
-    for (let i = 0; i < this.inputPaths.length; i++) {
-      symlinkOrCopySync(this.inputPaths[i], this.outputPath + '/' + i);
+  // This plugin writes foo.js into its outputPath
+  plugins.VeggiesPlugin = class VeggiesPlugin extends Plugin {
+    constructor(inputNodes, options) {
+      super(inputNodes || [], options);
+    }
+
+    build() {
+      fs.writeFileSync(this.outputPath + '/veggies.txt', 'tasty');
     }
   };
 
-  plugins.FailingPlugin = FailingPlugin;
-  FailingPlugin.prototype = Object.create(Plugin.prototype);
-  FailingPlugin.prototype.constructor = FailingPlugin;
-  function FailingPlugin(errorObject, options) {
-    Plugin.call(this, [], options);
-    this.errorObject = errorObject;
-  }
-  FailingPlugin.prototype.build = function() {
-    throw this.errorObject;
+  plugins.MergePlugin = class MergePlugin extends Plugin {
+    build() {
+      for (let i = 0; i < this.inputPaths.length; i++) {
+        symlinkOrCopySync(this.inputPaths[i], this.outputPath + '/' + i);
+      }
+    }
+  };
+
+  plugins.FailingPlugin = class FailingPlugin extends Plugin {
+    constructor(errorObject, options) {
+      super([], options);
+      this.errorObject = errorObject;
+    }
+
+    build() {
+      throw this.errorObject;
+    }
   };
 
   // Plugin for testing asynchrony. buildFinished is a deferred (RSVP.defer()).
   // The build will stall until you call node.finishBuild().
   // To wait until the build starts, chain on node.buildStarted.
   // Don't build more than once.
-  plugins.AsyncPlugin = AsyncPlugin;
-  AsyncPlugin.prototype = Object.create(Plugin.prototype);
-  AsyncPlugin.prototype.constructor = AsyncPlugin;
-  function AsyncPlugin(inputNodes, options) {
-    Plugin.call(this, inputNodes || [], options);
-    this.buildFinishedDeferred = RSVP.defer();
-    this.buildStartedDeferred = RSVP.defer();
-    this.buildStarted = this.buildStartedDeferred.promise;
-  }
-  AsyncPlugin.prototype.build = function() {
-    this.buildStartedDeferred.resolve();
-    return this.buildFinishedDeferred.promise;
-  };
-  AsyncPlugin.prototype.finishBuild = function(err) {
-    if (err != null) {
-      this.buildFinishedDeferred.reject(err);
-    } else {
-      this.buildFinishedDeferred.resolve();
+  plugins.AsyncPlugin = class AsyncPlugin extends Plugin {
+    constructor(inputNodes, options) {
+      super(inputNodes || [], options);
+      this.buildFinishedDeferred = RSVP.defer();
+      this.buildStartedDeferred = RSVP.defer();
+      this.buildStarted = this.buildStartedDeferred.promise;
+    }
+
+    build() {
+      this.buildStartedDeferred.resolve();
+      return this.buildFinishedDeferred.promise;
+    }
+
+    finishBuild(err) {
+      if (err != null) {
+        this.buildFinishedDeferred.reject(err);
+      } else {
+        this.buildFinishedDeferred.resolve();
+      }
     }
   };
 
-  plugins.SleepingPlugin = SleepingPlugin;
-  SleepingPlugin.prototype = Object.create(Plugin.prototype);
-  SleepingPlugin.prototype.constructor = SleepingPlugin;
-  function SleepingPlugin(inputNodes) {
-    Plugin.call(this, inputNodes || []);
-  }
-  SleepingPlugin.prototype.build = function() {
-    return new RSVP.Promise(resolve => setTimeout(resolve, 10));
+  plugins.SleepingPlugin = class SleepingPlugin extends Plugin {
+    constructor(inputNodes, options) {
+      super(inputNodes || [], options);
+    }
+
+    build() {
+      return new RSVP.Promise(resolve => setTimeout(resolve, 10));
+    }
   };
 
   return plugins;
