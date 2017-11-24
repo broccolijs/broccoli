f8d86068b925d81e17edb754f9744182166acf62
Stefan Penner <stefan.penner@gmail.com>
2017-11-24 10:04:50 -0800

    Separate describe block for `server`

--- test/cli_test.js
+++ test/cli_test.js
@@ -9,13 +9,11 @@ const cli = require('../lib/cli');
 chai.use(sinonChai);
 
 describe('cli', function() {
-  let mock = null;
   let oldCwd = null;
 
   beforeEach(function() {
     oldCwd = process.cwd();
     process.chdir('test/fixtures/project/subdir');
-    mock = sinon.mock(broccoli.server);
   });
 
   afterEach(function() {
@@ -23,36 +21,44 @@ describe('cli', function() {
     process.chdir(oldCwd);
   });
 
-  it('should start a server with default values', function() {
-    mock
-      .expects('serve')
-      .once()
-      .withArgs(sinon.match.any, sinon.match.string, sinon.match.number);
-    cli(['node', 'broccoli', 'serve']);
-    mock.verify();
-  });
+  describe('serve', function() {
+    let server;
 
-  it('starts server with given ip adress', function() {
-    mock.expects('serve').withArgs(sinon.match.any, '192.168.2.123', sinon.match.number);
-    cli(['node', 'broccoli', 'serve', '--host', '192.168.2.123']);
-    mock.verify();
-  });
+    beforeEach(function() {
+      server = sinon.mock(broccoli.server);
+    });
 
-  it('converts port to a number and starts the server at given port', function() {
-    mock
-      .expects('serve')
-      .once()
-      .withArgs(sinon.match.any, sinon.match.string, 1234);
-    cli(['node', 'broccoli', 'serve', '--port', '1234']);
-    mock.verify();
-  });
+    it('should start a server with default values', function() {
+      server
+        .expects('serve')
+        .once()
+        .withArgs(sinon.match.any, sinon.match.string, sinon.match.number);
+      cli(['node', 'broccoli', 'serve']);
+      server.verify();
+    });
 
-  it('converts port to a number and starts the server at given port and host', function() {
-    mock
-      .expects('serve')
-      .once()
-      .withArgs(sinon.match.any, '192.168.2.123', 1234);
-    cli(['node', 'broccoli', 'serve', '--port=1234', '--host=192.168.2.123']);
-    mock.verify();
+    it('starts server with given ip adress', function() {
+      server.expects('serve').withArgs(sinon.match.any, '192.168.2.123', sinon.match.number);
+      cli(['node', 'broccoli', 'serve', '--host', '192.168.2.123']);
+      server.verify();
+    });
+
+    it('converts port to a number and starts the server at given port', function() {
+      server
+        .expects('serve')
+        .once()
+        .withArgs(sinon.match.any, sinon.match.string, 1234);
+      cli(['node', 'broccoli', 'serve', '--port', '1234']);
+      server.verify();
+    });
+
+    it('converts port to a number and starts the server at given port and host', function() {
+      server
+        .expects('serve')
+        .once()
+        .withArgs(sinon.match.any, '192.168.2.123', 1234);
+      cli(['node', 'broccoli', 'serve', '--port=1234', '--host=192.168.2.123']);
+      server.verify();
+    });
   });
 });
