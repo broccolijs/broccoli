51418c0ba7a6e10f740fd2ee2307f5b9e9bfd6e3
Travis Hoover <thoov7@gmail.com>
2019-05-15 13:24:29 -0700

    Adding better logging

--- test/wrappers_test.js
+++ test/wrappers_test.js
@@ -10,6 +10,7 @@ describe('transform-node', function() {
   beforeEach(() => {
     transform = new TransformNode();
     transform.nodeInfo = {
+      persistentOutput: true,
       setup() {},
       getCallbackObject() {
         return this;
@@ -24,16 +25,38 @@ describe('transform-node', function() {
     delete process.env['BROCCOLI_VOLATILE'];
   });
 
+  it('memoizationEnabled method should return the correct value', function() {
+    process.env['BROCCOLI_VOLATILE'] = true;
+    expect(transform.memoizationEnabled(false)).to.be.true;
+
+    process.env['BROCCOLI_VOLATILE'] = false;
+    expect(transform.memoizationEnabled(true)).to.be.true;
+
+    process.env['BROCCOLI_VOLATILE'] = true;
+    expect(transform.memoizationEnabled()).to.be.true;
+
+    process.env['BROCCOLI_VOLATILE'] = false;
+    expect(transform.memoizationEnabled(false)).to.be.false;
+
+    delete process.env['BROCCOLI_VOLATILE'];
+    expect(transform.memoizationEnabled(false)).to.be.false;
+
+    delete process.env['BROCCOLI_VOLATILE'];
+    expect(transform.memoizationEnabled()).to.be.false;
+  });
+
   it('shouldBuild method should return false there are no input nodes and this is a rebuild', function() {
     process.env['BROCCOLI_VOLATILE'] = true;
-    transform._revision = 1;
 
+    transform._revision = 0;
+    expect(transform.shouldBuild()).to.be.true;
+
+    transform._revision = 1;
     expect(transform.shouldBuild()).to.be.false;
   });
 
   it('shouldBuild method should return false if none of the inputs changed', function() {
     process.env['BROCCOLI_VOLATILE'] = true;
-    transform.nodeInfo.persistentOutput = true;
 
     let inputWrapperA = new Node();
     let inputWrapperB = new Node();
@@ -48,8 +71,6 @@ describe('transform-node', function() {
   it('shouldBuild method should return true if some of the inputs changed', function() {
     process.env['BROCCOLI_VOLATILE'] = true;
 
-    transform.nodeInfo.persistentOutput = true;
-
     let inputWrapperA = new Node();
     let inputWrapperB = new Node();
 
@@ -58,6 +79,38 @@ describe('transform-node', function() {
     return transform.build().then(() => {
       inputWrapperA.revise();
       expect(transform.shouldBuild()).to.be.true;
+    });
+  });
+
+  it('if volatile is set to true then do not call revise', function() {
+    transform.nodeInfo.volatile = true;
+
+    let inputWrapperA = new Node();
+    let inputWrapperB = new Node();
+
+    transform.inputNodeWrappers = [inputWrapperA, inputWrapperB];
+
+    inputWrapperA.revise();
+    expect(transform.revision).to.equal(0);
+
+    return transform.build().then(() => {
+      expect(transform.revision).to.equal(0);
+    });
+  });
+
+  it('if volatile is not set (or false) then revise is automatically called', function() {
+    transform.nodeInfo.volatile = false;
+
+    let inputWrapperA = new Node();
+    let inputWrapperB = new Node();
+
+    transform.inputNodeWrappers = [inputWrapperA, inputWrapperB];
+
+    inputWrapperA.revise();
+    expect(transform.revision).to.equal(0);
+
+    return transform.build().then(() => {
+      expect(transform.revision).to.equal(1);
     });
   });
 });
