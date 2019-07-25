b661465edf972c22f389d33e0b37aef21ca0e6c2
Stefan Penner <stefan.penner@gmail.com>
2019-07-25 12:40:43 -0700

    Choose better data structure for nodeWrappers.
    Since NodeWrappers lists:
    
    * must be insertion ordered
    * must contain only unique values (based on their key)
    * support a fast existence / lookup operation
    
    This PR switches it from an array to a map.
    
    As broccoli pipelines grow large, simply searching the array to detect duplicates on insertion can become very costly, and switching to a Map addresses this specific bottleneck.

---
+++ test/utils/filter_map_test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const filterMap = require('../../lib/utils/filter-map');
+
+describe('filterMap', function() {
+  it('works', function() {
+    expect(filterMap([], () => true)).to.eql([]);
+    expect(filterMap([1, false, 2], () => true)).to.eql([1, false, 2]);
+    expect(filterMap([1, true, 2], () => false)).to.eql([]);
+    expect(filterMap([1, true, 2], x => x === 1)).to.eql([1]);
+    expect(filterMap([1, true, 2], x => typeof x === 'number')).to.eql([1, 2]);
+  });
+});
