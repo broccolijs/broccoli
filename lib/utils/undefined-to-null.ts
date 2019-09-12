5a0ac4a79e65339207a3e8c192de45668b9cb57f
Travis Hoover <thoov7@gmail.com>
2019-09-12 01:42:14 -0700

    Fix any type

--- lib/utils/undefined-to-null.ts
+++ lib/utils/undefined-to-null.ts
@@ -1,5 +1,5 @@
 // Replace all `undefined` values with `null`, so that they show up in JSON output
-export default function undefinedToNull(obj: { [index: string]: NonNullable<any> }) {
+export default function undefinedToNull(obj: { [index: string]: any }) {
   for (const key in obj) {
     if (obj.hasOwnProperty(key) && obj[key] === undefined) {
       obj[key] = null;
