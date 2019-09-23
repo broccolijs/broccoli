1718850a6e9c33bfe445712edb39008be081793b
Travis Hoover <thoov7@gmail.com>
2019-09-23 15:51:34 -0700

    Removing debuggers

--- lib/index.ts
+++ lib/index.ts
@@ -1,30 +1,23 @@
 export = {
   get Builder() {
-    debugger;
     return require('./builder');
   },
   get loadBrocfile() {
-    debugger;
     return require('./load_brocfile');
   },
   get server() {
-    debugger;
     return require('./server');
   },
   get getMiddleware() {
-    debugger;
     return require('./middleware');
   },
   get Watcher() {
-    debugger;
     return require('./watcher');
   },
   get WatcherAdapter() {
-    debugger;
     return require('./watcher_adapter');
   },
   get cli() {
-    debugger;
     return require('./cli');
   },
 };
